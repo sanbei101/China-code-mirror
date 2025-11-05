44e99e1bced86e563ffbf20454ce1d817c3491ea
Sanbei <sanbei101@outlook.com>
2025-11-05 14:29:35 +0800

    Update install-docker.sh

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -10,11 +10,9 @@ wget -O- https://gitee.com/tech-shrimp/docker_installer/releases/download/latest
 
 # 第三步：更改 Docker 镜像源
 echo "更改 Docker 镜像源..."
-sudo tee /etc/docker/daemon.json > /dev/null <<EOF
-{
+echo '{
   "registry-mirrors": ["https://docker.sanbei101.xyz"]
-}
-EOF
+}' | sudo tee /etc/docker/daemon.json > /dev/null
 
 # 第四步：重启 Docker 服务
 echo "重启 Docker 服务..."
